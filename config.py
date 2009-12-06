9c638c078dae7356c92419409d6506024e410082
Anh Hai Trinh <anh.hai.trinh@gmail.com>
2009-12-06 23:33:16 +0700

    Restyling complete

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Name of the blog
-blog_name = 'My Blog'
-
-# Your name (used for copyright info)
-author_name = 'the author'
+blog_name = 'On ideas worth stealing'
 
 # (Optional) slogan
-slogan = 'This is my blog'
+slogan = "A thief's perspective"
+
+# Your name (used for copyright info)
+author_name = 'Anh Hai Trinh'
 
 # The hostname this site will primarially serve off (used for Atom feeds)
-host = 'localhost'
+host = 'blog.onideas.ws'
 
 # Selects the theme to use. Theme names correspond to directories under
 # the 'themes' directory, containing templates and static content.
@@ -19,18 +19,20 @@ theme = 'default'
 #   year - the year the post was published in
 #   month - the month the post was published in
 #   day - the day the post was published in
-post_path_format = '/%(year)d/%(month)02d/%(slug)s'
+post_path_format = '/%(slug)s'
 
 # A nested list of sidebar menus, for convenience. If this isn't versatile
 # enough, you can edit themes/default/base.html instead.
 sidebars = [
-  ('Blogroll', [
+  ("Identity", [
+    ('github', 'http://github.com/aht'),
+    ('twitter', 'http://twitter.com/chickamade'),
+  ]),
+  ('Inspiration', [
+    ('good/bad math', 'http://scienceblogs.com/goodmath/'),
     ('Nick Johnson', 'http://blog.notdot.net/'),
-    ('Bill Katz', 'http://www.billkatz.com/'),
-    ('Coding Horror', 'http://www.codinghorror.com/blog/'),
-    ('Craphound', 'http://craphound.com/'),
-    ('Neopythonic', 'http://www.neopythonic.blogspot.com/'),
-    ('Schneier on Security', 'http://www.schneier.com/blog/'),
+    ('Paul Graham', 'http://paulgraham.com/articles.html'),
+    ('research!rsc', 'http://research.swtch.com/'),
   ]),
 ]
 
@@ -42,13 +44,13 @@ html_mime_type = "text/html; charset=utf-8"
 
 # To use disqus for comments, set this to the 'short name' of the disqus forum
 # created for the purpose.
-disqus_forum = None
+disqus_forum = 'onideas'
 
 # Length (in words) of summaries, by default
-summary_length = 200
+summary_length = 100
 
 # If you want to use Google Analytics, enter your 'web property id' here
-analytics_id = None
+analytics_id = 'UA-11948966-1'
 
 # If you want to use PubSubHubbub, supply the hub URL to use here.
 hubbub_hub_url = 'http://pubsubhubbub.appspot.com/'
@@ -58,13 +60,7 @@ hubbub_hub_url = 'http://pubsubhubbub.appspot.com/'
 # an html file' method, then set the NAME of the file below.
 # Note that you do not need to download the file provided - just enter its name
 # here.
-google_site_verification = None
+google_site_verification = 'google4b7548b2151a637a.html'
 
 # Default markup language for entry bodies (defaults to html).
 default_markup = 'html'
-
-# Syntax highlighting style for RestructuredText and Markdown,
-# one of 'manni', 'perldoc', 'borland', 'colorful', 'default', 'murphy',
-# 'vs', 'trac', 'tango', 'fruity', 'autumn', 'bw', 'emacs', 'pastie',
-# 'friendly', 'native'.
-highlighting_style = 'friendly'
